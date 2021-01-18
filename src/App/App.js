a88d64aa450d09560360c34879539193bdf2e619
Issa sangare <issabmsangare@hotmail.com>
2021-01-18 13:14:59 -0500

    styling app componentwith ThemeProvider and createMuiTheme

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,21 @@
 import react from "react"
 import './App.css';
 import SideMenu from "../components/SideMenu"
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import { CssBaseline, makeStyles, createMuiTheme, ThemeProvider } from "@material-ui/core";
 import Header from "../components/Header"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -14,13 +27,13 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    < >
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider  >
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,25 @@ import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import SearchIcon from '@material-ui/icons/Search';
-const useStyles = makeStyles({
+
+
+const useStyles = makeStyles(theme => ({
     root: {
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        
     },
-    searchInput:{
-        opacity:'0.6',
-        padding: '0px 8px',
+    searchInput: {
+        opacity: '0.6',
+        padding: `0px ${theme.spacing(1)}px`,
         fontSize: '0.8rem',
-        "&:hover":{
+        '&:hover': {
             backgroundColor: '#f2f2f2'
         },
-        
         '& .MuiSvgIcon-root': {
-            marginRight: '8px'
+            marginRight: theme.spacing(1)
         }
     }
-})
+}))
 
 
 
@@ -42,7 +44,7 @@ function Header() {
                 </Grid>
                 <Grid item sm ></Grid>
                 <Grid item  >
-                    <IconButton>
+                    <IconButton >
                         <Badge bagdeContent={4} color="secondary">
                             <NotificationsNoneIcon ontSize="small"  />
                         </Badge>                      
